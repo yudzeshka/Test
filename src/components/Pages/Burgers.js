7597b4d259866d2cc1ed502e49d23f120afd9368
Maksim Yudzeshka <yudzeshka@gmail.com>
2022-01-26 09:37:29 +0300

    Bugs fixed

--- src/components/Pages/Burgers.js
+++ src/components/Pages/Burgers.js
@@ -11,7 +11,6 @@ export default function Burgers({ API }) {
   const isCart = false;
 
   React.useEffect(() => {
-    console.log("start loading");
     axios
       .get(`${API}/Burgers`)
       .then(({ data }) => setBurgers(data))
@@ -20,7 +19,6 @@ export default function Burgers({ API }) {
           setIsLoading(false);
         }, 1300)
       );
-    console.log("finish loading");
   }, []);
   const onAddToCart = (obj) => {
     axios.post(`${API}/Cart`, obj);
